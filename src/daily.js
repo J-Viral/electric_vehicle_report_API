6187cf87c49683b0c88914c185fc1bbc9010f86e
J-Viral <30624865+J-Viral@users.noreply.github.com>
2024-04-19 18:06:03 +0530

    Update daily.js

--- src/daily.js
+++ src/daily.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 
 // Create a single supabase client for interacting with your database
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_key;
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndhanNkcWZyeXFqcnFiemFpdm5jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMzNDkyOTksImV4cCI6MjAyODkyNTI5OX0.2TnqT-QqiYYspw4H-akhLN5wXtDuXhrvW2P_GEOi-O4";
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 const daily = async (req, res) => {
